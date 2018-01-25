938ded8a305d4d198fa95c05fda1c229fd4846de
Usuario <Usuario@172.30.3.59>
2018-01-25 17:54:54 -0500

    Jos

--- Taller16ene/src/taller16ene/rnegocio/dao/IVehiculo.java
+++ Taller16ene/src/taller16ene/rnegocio/dao/IVehiculo.java
@@ -1,13 +1,13 @@
 package taller16ene.rnegocio.dao;
 
 import java.util.*;
-import taller16ene.rnegocio.Impl.Cooperativa;
-import taller16ene.rnegocio.Impl.Vehiculo;
+
+import taller16ene.rnegocio.Impl.VehiculoImp;
 
 public interface IVehiculo {
-    public int insertar (Vehiculo vehiculo) throws Exception;
-    public int modificar (Vehiculo vehiculo) throws Exception; 
-    public int eliminar (Vehiculo vehiculo) throws Exception;
-    public Cooperativa obtener (long id) throws Exception;
-    public ArrayList<Vehiculo> obtener() throws Exception;
+    public int insertar (VehiculoImp vehiculo) throws Exception;
+    public int modificar (VehiculoImp vehiculo) throws Exception; 
+    public int eliminar (VehiculoImp vehiculo) throws Exception;
+    public VehiculoImp obtener (int Id_Coop) throws Exception;
+    public ArrayList<VehiculoImp> obtener() throws Exception;
 }
