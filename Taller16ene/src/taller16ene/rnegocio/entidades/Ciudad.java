a41d72be0f585cef3fbd24c350e13e50d6ca7b88
FabricioConstante <Usuario@FI-LABC01-PC01>
2018-01-25 18:07:32 -0500

    entidad etiquetas

---
+++ Taller16ene/src/taller16ene/rnegocio/entidades/Ciudad.java
@@ -0,0 +1,44 @@
+package taller16ene.rnegocio.entidades;
+import java.util.Date;
+public class Ciudad {
+    private int idciudad;
+    private String nombre;
+    private String provincia;
+    private String telefono;
+    private String direccion;
+
+    public Ciudad() {
+    }
+ 
+    public Ciudad(int idciudad, String nombre, String provincia) {
+        this.idciudad = idciudad;
+        this.nombre = nombre;
+        this.provincia = provincia;
+    }
+
+    public int getIdciudad() {
+        return idciudad;
+    }
+
+    public void setIdciudad(int idciudad) {
+        this.idciudad = idciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    
+}
