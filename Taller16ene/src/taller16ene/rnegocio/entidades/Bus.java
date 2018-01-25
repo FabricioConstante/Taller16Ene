b4c45521771c1a0e900e1e0cd0074442388b607b
FabricioConstante <Usuario@FI-LABC01-PC01>
2018-01-25 18:27:11 -0500

    entidad etiquetas

---
+++ Taller16ene/src/taller16ene/rnegocio/entidades/Bus.java
@@ -0,0 +1,51 @@
+package taller16ene.rnegocio.entidades;
+import java.util.Date;
+public class Bus {
+    private int idbus;
+    private Ciudad idciudad;    
+    //private Cooperativa idcoop;
+    private String horario;
+
+    public Bus() {
+    }
+
+    public Bus(int idbus, Ciudad idciudad, /*Cooperativa idcoop,*/ String horario) {
+        this.idbus = idbus;
+        this.idciudad = idciudad;
+        //this.idcoop = idcoop;
+        this.horario = horario;
+    }
+
+    public int getIdbus() {
+        return idbus;
+    }
+
+    public void setIdbus(int idbus) {
+        this.idbus = idbus;
+    }
+
+    public Ciudad getIdciudad() {
+        return idciudad;
+    }
+
+    public void setIdciudad(Ciudad idciudad) {
+        this.idciudad = idciudad;
+    }
+    
+//    public Cooperativa getIdcoop() {
+//        return idciudad;
+//    }
+//
+//    public void setIdcoop(Cooperativa idcoop) {
+//        this.idcoop = idcoop;
+//    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+ 
+}
