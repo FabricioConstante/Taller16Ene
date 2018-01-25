1fd463773584cd3bac846499edeefbd4e5660a36
Usuario <Usuario@172.30.3.59>
2018-01-25 17:56:56 -0500

    Jos

--- Taller16ene/src/taller16ene/rnegocio/dao/ICooperativa.java
+++ Taller16ene/src/taller16ene/rnegocio/dao/ICooperativa.java
@@ -1,12 +1,12 @@
 package taller16ene.rnegocio.dao;
 
 import java.util.*;
-import taller16ene.rnegocio.Impl.Cooperativa;
+import taller16ene.rnegocio.Impl.CooperativaImp;
 
 public interface ICooperativa {
-    public int insertar (Cooperativa cooperativa) throws Exception;
-    public int modificar (Cooperativa cooperativa) throws Exception; 
-    public int eliminar (Cooperativa cooperativa) throws Exception;
-    public Cooperativa obtener (long id) throws Exception;
-    public ArrayList<Cooperativa> obtener() throws Exception;
+    public int insertar (CooperativaImp cooperativa) throws Exception;
+    public int modificar (CooperativaImp cooperativa) throws Exception; 
+    public int eliminar (CooperativaImp cooperativa) throws Exception;
+    public CooperativaImp obtener (int Id_Coop) throws Exception;
+    public ArrayList<CooperativaImp> obtener() throws Exception;
 }
